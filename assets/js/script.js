a9b828958c358a37fe5585d797354b93691b4e73
notalisk <81662512+notalisk@users.noreply.github.com>
2023-04-15 19:02:21 -0500

    add function to pull events from storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
   var today = dayjs();
 
@@ -46,14 +25,45 @@ $(function () {
       calEvents.push(thisEvent);
       calEvents.splice(0, 1);
     }
-    calEvents.push(thisEvent);
 
     localStorage.setItem("calendarEvents", JSON.stringify(calEvents));
+
+    render();
   });
 
   // check local storage for stored calendar entries
-  function renderUponLoad () {
+  function render () {
     var calEvents = JSON.parse(localStorage.getItem("calendarEvents"));
+
+    for (var i = 0; i < calEvents.length; i++) {
+      var eventTime = "hour-" + calEvents[i].time;
+
+      var textareaEl = document.getElementById(eventTime).children[1];
+      var content = calEvents[i].eventName;
+
+      textareaEl.value = content;
+    }
+
+     compare();
   }
 
+  render();
+
+  // timer
+  var timerInterval = setInterval(render, 10000);
+
+  function compare() {
+    var blocks = $("button").parent();
+    var ourHour = today.format("H");
+
+    for (var i = 0; i < blocks.length; i++) {
+      if (ourHour == i + 9) {
+        blocks[i].className = "row time-block present";
+      } else if (ourHour < i + 9) {
+        blocks[i].className = "row time-block future";
+      } else if (ourHour > i + 9) {
+        blocks[i].className = "row time-block past";
+      }
+    }
+  }
 });
